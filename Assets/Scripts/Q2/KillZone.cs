d787399d54a1c25ecbfda569af9f774e45f2ba70
caprexy <caprexy@yahoo.com>
2023-01-23 19:12:43 -0600

    Q2

---
+++ Assets/Scripts/Q2/KillZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Q2 { 
+    public class KillZone : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<CharacterController>())
+            {
+                string currentScene = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(currentScene);
+            }
+        }
+    }
+}
